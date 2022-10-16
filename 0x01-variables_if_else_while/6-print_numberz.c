1855f14bc64168d31c541c86078834664cfa7045
oshittu6 <lanreshittu.ldc@gmail.com>
2022-10-16 10:00:10 -0700

    print num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - printing numbers
+ *
+ * Return:0(sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
