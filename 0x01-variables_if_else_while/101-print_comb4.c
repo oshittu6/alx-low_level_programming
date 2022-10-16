a1a64c7dc5c683e8a7fbf4bb412b345244488b2c
oshittu6 <lanreshittu.ldc@gmail.com>
2022-10-16 11:24:20 -0700

    Advance task 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - printing combinations of 3 digit
+ * main - printing num comb
  *
  * Return:0(sucess)
  */
 int main(void)
 {
 	int i, j, k;
-	for (i = '0'; i <= '9'; i++)
+
+	for (i = 48; i < 57; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 48; j < 57; j++)
 		{
-			for (k = '0'; k <= '9'; k++)
+			for (k = 48; k <= 57; k++)
 			{
-				if (i < j && j < k)
+				if (j > i && k > j)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
-					if (i == '7' && j == '8' && k == '9')
-					{
-						break;
-					}
-					else
+					if (i != 55 || j != 56 || k != 57)
 					{
 						putchar(',');
-						putchar('');
+						putchar(' ');
 					}
 				}
 			}
