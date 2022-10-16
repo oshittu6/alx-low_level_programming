48978350a9246f666e34132e4e3190854b18711f
oshittu6 <lanreshittu.ldc@gmail.com>
2022-10-16 09:39:39 -0700

    ABC

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - printing a-z
+ *
+ * Return:0(sucess)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
