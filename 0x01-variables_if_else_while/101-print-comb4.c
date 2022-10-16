69cc01b1bedfc1c0921208c5f3c8727125830ca0
oshittu6 <lanreshittu.ldc@gmail.com>
2022-10-16 10:48:51 -0700

    Advance task2

---
+++ 0x01-variables_if_else_while/101-print-comb4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - printing a-z
+ *
+ * Return:0(sucess)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = 48; j < 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (j > i && k > j)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56 || k != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
