f131b69b59c40bcdcfd987362093c722d91c7f80
oshittu6 <lanreshittu.ldc@gmail.com>
2022-10-19 07:32:35 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,15 @@
  * Return: 0 (success)
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
-	int count = 0
+	int count = 0;
 	char let;
 
 	while (count++ <= 9)
-	for (let = 'a'; let <= 'z'; let++)
 	{
-		_putchar(let);
+		for (let = 'a'; let <= 'z'; let++)
+			_putchar(let);
 		_putchar('\n');
 	}
 }
