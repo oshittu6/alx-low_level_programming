0e2282b7e6092c171223cac25e3cdf3a6a6b8314
oshittu6 <lanreshittu.ldc@gmail.com>
2022-10-19 06:48:45 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - printing 10x
+ * print_alphabet_x10 - print 10x alphabet
+ * Return: 0 (success)
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+
+	for (i = 'a'; i = 'z'; i++)
+	{
+		_putchar(i);
+	}
+}
+int main(void)
+{
+	print_alphabet_x10(void);
+	print_alphabet_x10(void);
+	print_alphabet_x10(void);
+	print_alphabet_x10(void);
+	print_alphabet_x10(void);
+	print_alphabet_x10(void);
+	print_alphabet_x10(void);
+	print_alphabet_x10(void);
+	print_alphabet_x10(void);
+	print_alphabet_x10(void);
+	_putchar('\n')
+	return (0);
+}
