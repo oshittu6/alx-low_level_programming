0768ba10352a070f41d3d0f1fb35a6b880eda534
oshittu6 <lanreshittu.ldc@gmail.com>
2022-10-21 00:45:26 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print num from 0-9
+ * Return: 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	j = 9
+
+	for (i = 0; j <= 9; i++)
+	{
+		_putchar(i);
+	}
+	-putchar('\n')
+	return (0);
+}
